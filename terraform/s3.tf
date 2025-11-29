ab06d834189f9d7627471d1c461b7ecf0cf3d49c
darlavaishnavi <darlavaishnavi27@gmail.com>
2025-11-29 01:07:29 -0500

    Add S3 bucket and versioning configuration

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,1 +1,12 @@
+resource "aws_s3_bucket" "media" {
+  bucket = "strapi-media-${var.environment}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "versioning" {
+  bucket = aws_s3_bucket.media.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
 
