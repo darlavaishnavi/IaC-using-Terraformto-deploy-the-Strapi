2d0dfcc7ef298716c33ff971a008e21a5a1d7f8e
darlavaishnavi <darlavaishnavi27@gmail.com>
2025-11-29 01:12:10 -0500

    Add IAM role and policy for ECS task

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,1 +1,28 @@
+resource "aws_iam_role" "ecs_task_role" {
+  name = "strapi-task-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_policy" "s3_policy" {
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = ["s3:*"],
+      Effect = "Allow",
+      Resource = "*"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach" {
+  role       = aws_iam_role.ecs_task_role.name
+  policy_arn = aws_iam_policy.s3_policy.arn
+}
 
