e1be286457cc0b00bfc86abc6dd93210b79e5402
darlavaishnavi <darlavaishnavi27@gmail.com>
2025-11-29 01:07:01 -0500

    Add Terraform variables for AWS and database configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,15 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
 
+variable "environment" {
+  default = "prod"
+}
+
+variable "db_username" {
+  default = "strapi"
+}
+
+variable "db_allocated_storage" {
+  default = 20
+}
