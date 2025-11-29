9502426b3bc69488fe0d43f0b3ee48208b58b193
darlavaishnavi <darlavaishnavi27@gmail.com>
2025-11-29 01:03:39 -0500

    Configure AWS S3 upload provider in plugins.js

--- app/plugins.js
+++ app/plugins.js
@@ -1,1 +1,16 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "@strapi/provider-upload-aws-s3",
+      providerOptions: {
+        s3: {
+          region: env("AWS_REGION"),
+          params: {
+            Bucket: env("AWS_S3_BUCKET"),
+          },
+        },
+      },
+    },
+  },
+});
 
