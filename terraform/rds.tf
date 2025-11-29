9790b6f642a524119afd8aaed47fbed41745d9c0
darlavaishnavi <darlavaishnavi27@gmail.com>
2025-11-29 01:07:49 -0500

    Add RDS instance resource for Strapi database

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,1 +1,11 @@
+resource "aws_db_instance" "strapi_db" {
+  identifier           = "strapi-db"
+  engine               = "postgres"
+  instance_class       = "db.t3.micro"
+  allocated_storage    = var.db_allocated_storage
+  username             = var.db_username
+  password             = "ChangeMe123!"
+  publicly_accessible  = false
+  skip_final_snapshot  = true
+}
 
