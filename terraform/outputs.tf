d5637d554de30020281fc264c084f8559a99a453
darlavaishnavi <darlavaishnavi27@gmail.com>
2025-11-29 01:13:02 -0500

    Add outputs for ALB DNS, RDS endpoint, and S3 bucket

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,12 @@
+output "alb_dns" {
+  value = aws_lb.strapi_lb.dns_name
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.strapi_db.address
+}
+
+output "s3_bucket" {
+  value = aws_s3_bucket.media.bucket
+}
 
